efdf3e706d5971e5f10c51e2939aaed833696af1
Matthijs G. Snijder <snijdermatthijs@gmail.com>
2024-07-05 15:56:35 +0200

    add basic resume component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,4 @@
 import { Routes } from '@angular/router';
-import { TodayComponent } from './today/today.component';
-import { AnotherDayComponent } from './another-day/another-day.component';
-import { TimeBanditComponent } from './time-bandit/time-bandit.component';
+import { ResumeComponent } from './resume/resume.component';
 
-export const routes: Routes = [
-  { path: 'today', component: TodayComponent },
-  { path: 'another-day', component: AnotherDayComponent },
-  { path: 'time-bandit', component: TimeBanditComponent },
-];
+export const routes: Routes = [{ path: 'resume', component: ResumeComponent }];
